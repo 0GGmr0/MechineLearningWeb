78db2d2780b3c34a2b7a17ef39a00d16eeec8346
0GGmr0 <18817619557@163.com>
2018-05-14 10:34:17 +0800

    增加热部署配置

---
+++ src/main/java/com/web/machineversion/HelloController.java
@@ -0,0 +1,16 @@
+package com.web.machineversion;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "say", method = RequestMethod.GET)
+    public String say(HttpServletRequest request) {
+        request.setAttribute("say", "heelasdasdasdo");
+        return "test";
+    }
+}
