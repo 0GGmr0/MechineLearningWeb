cb4c2eba38c2b3e492c0ff5b97ccb4b62a972116
angelalife <756429783@qq.com>
2018-05-28 16:48:34 +0800

    日程板块

---
+++ src/main/java/com/web/machineversion/service/Impl/DayTaskServiceImpl.java
@@ -0,0 +1,10 @@
+package com.web.machineversion.service.Impl;
+
+import com.web.machineversion.service.DayTaskService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class DayTaskServiceImpl implements DayTaskService {
+}
